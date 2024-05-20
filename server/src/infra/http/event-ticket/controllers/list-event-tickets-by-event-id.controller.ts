b73a3b95d61eb8c26d2032595ec4790dd662d83e
Kemuel Batista <kemuellima20@gmail.com>
2024-05-20 09:05:16 -0300

    feat: list event ticket by event ID controller and test

---
+++ server/src/infra/http/event-ticket/controllers/list-event-tickets-by-event-id.controller.ts
@@ -0,0 +1,33 @@
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common'
+
+import {
+  ParamsSchema,
+  paramsValidationPipe,
+} from '@/core/schemas/params-schema'
+import { ListEventTicketsByEventIdUseCase } from '@/domain/admsjp/use-cases/event-ticket/list-event-tickets-by-event-id'
+import { Public } from '@/infra/auth/public'
+
+@Controller('/event/:id')
+@Public()
+export class ListEventTicketsByEventIdController {
+  constructor(
+    private listEventTicketsByEventIdUseCase: ListEventTicketsByEventIdUseCase,
+  ) {}
+
+  @Get()
+  async handle(@Param('id', paramsValidationPipe) id: ParamsSchema) {
+    const result = await this.listEventTicketsByEventIdUseCase.execute({
+      eventId: id,
+    })
+
+    if (result.isError()) {
+      throw new BadRequestException()
+    }
+
+    const eventTickets = result.value.eventTickets
+
+    return {
+      eventTickets,
+    }
+  }
+}
--- server/src/infra/http/event-ticket/event-ticket-http.module.ts
+++ server/src/infra/http/event-ticket/event-ticket-http.module.ts
@@ -1,16 +1,25 @@
 import { Module } from '@nestjs/common'
 
 import { CreateEventTicketUseCase } from '@/domain/admsjp/use-cases/event-ticket/create-event-ticket'
+import { ListEventTicketsByEventIdUseCase } from '@/domain/admsjp/use-cases/event-ticket/list-event-tickets-by-event-id'
 import { RegisterEventQueue } from '@/domain/admsjp/use-cases/queues/register-event-queue'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { GeneratorsModule } from '@/infra/generators/generators.module'
 import { QueueModule } from '@/infra/queue/queue.module'
 
 import { CreateEventTicketController } from './controllers/create-event-ticket.controller'
+import { ListEventTicketsByEventIdController } from './controllers/list-event-tickets-by-event-id.controller'
 
 @Module({
   imports: [DatabaseModule, QueueModule, GeneratorsModule],
-  controllers: [CreateEventTicketController],
-  providers: [CreateEventTicketUseCase, RegisterEventQueue],
+  controllers: [
+    CreateEventTicketController,
+    ListEventTicketsByEventIdController,
+  ],
+  providers: [
+    CreateEventTicketUseCase,
+    ListEventTicketsByEventIdUseCase,
+    RegisterEventQueue,
+  ],
 })
 export class EventTicketHttpModule {}
