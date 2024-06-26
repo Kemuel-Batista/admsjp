f61a9cae845e77e776bea384845d60e8c2832a0b
Kemuel Batista <kemuellima20@gmail.com>
2024-06-26 13:15:52 -0300

    fix: send email

--- server/scripts/send_email.py
+++ server/scripts/send_email.py
@@ -3,13 +3,13 @@ import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-from datetime import date
+from datetime import datetime
 
 # Configurações do servidor SMTP do Gmail
 smtp_server = "smtp.gmail.com"
 smtp_port = 587
 smtp_username = "kemuellima20@gmail.com"  # Insira aqui o seu email do Gmail
-smtp_password = "roezgbcoskjtcozs "  # Insira aqui a senha do seu email do Gmail
+smtp_password = "roezgbcoskjtcozs"  # Insira aqui a senha do seu email do Gmail
 
 # Função para enviar email com os arquivos em anexo
 def send_email(subject, body, sender_email, recipient_email, attachment_folder):
@@ -41,8 +41,10 @@ def send_email(subject, body, sender_email, recipient_email, attachment_folder):
 
 if __name__ == "__main__":
     # Configurações do email
-    current_date = date.today().strftime("%Y-%m-%d")
-    subject = f"Arquivos de Backup - ADMSJP - {current_date}"
+    current_datetime = datetime.now()
+    current_date = current_datetime.strftime("%Y-%m-%d")
+    current_hour = current_datetime.strftime("%H-%M")
+    subject = f"Arquivos de Backup - ADMSJP - {current_date} - {current_hour}"
     body = "Segue anexado os arquivos de backup do app ADMSJP."
     sender_email = smtp_username
     recipient_email = "kemuellima20@gmail.com"  # Insira aqui o email de destino
@@ -50,10 +52,10 @@ if __name__ == "__main__":
     # Pasta de destino do backup gerado pelo script em bash
     backup_folder = "/projects/backup"
 
-    # Criar o nome da pasta de backup do dia atual
-    backup_folder_today = f"admsjp-{current_date}_00-00"
+    # Criar o nome da pasta de backup do dia e hora atuais
+    backup_folder_today = f"admsjp-{current_date}_{current_hour}"
 
-    # Caminho completo da pasta de backup do dia atual
+    # Caminho completo da pasta de backup do dia e hora atuais
     attachment_folder = os.path.join(backup_folder, backup_folder_today)
 
     # Envia o email com os arquivos em anexo
