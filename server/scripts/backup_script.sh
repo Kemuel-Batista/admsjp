3b9de24c3b71f974b4bd96a3a6e0a50e50a8a4ac
Kemuel Batista <kemuellima20@gmail.com>
2024-06-26 12:51:59 -0300

    fix: script

--- server/scripts/backup_script.sh
+++ server/scripts/backup_script.sh
@@ -22,7 +22,7 @@ if [ ! -d ${PBACKUP} ]; then
 	echo ""
 else
 	echo ""
-	echo " Rotacionando backups mais antigos que $NDIAS"
+	echo " Rotacionando backups mais antigos que $NDIAS dias"
 	echo ""
 
 	find ${PBACKUP} -type d -mtime +$NDIAS -exec rm -rf {} \;
@@ -31,47 +31,42 @@ fi
 
 ### Postgres
 
-if [ ! -d $PBACKUP/admsjp-$DATA ]; then
-  mkdir -p $PBACKUP/admsjp-$DATA/
+BACKUP_DIR=$PBACKUP/admsjp-$DATA
+if [ ! -d $BACKUP_DIR ]; then
+  mkdir -p $BACKUP_DIR
 fi
 
-chown -R postgres:postgres $PBACKUP/admsjp-$DATA/
-
+# Ajusta as permissões da pasta de backup
+chown -R postgres:postgres $BACKUP_DIR
 
 su - postgres -c "vacuumdb -a -f -z"
 
 for basepostgres in $(su - postgres -c "psql -l" | grep -v template0 | grep -v template1 | grep -v restaurantsystem | grep -v postgres | grep "|" |grep -v Owner |awk '{if ($1 != "|" && $1 != "Nome") print $1}'); do
-  su - postgres -c "pg_dump $basepostgres > $PBACKUP/admsjp-$DATA/$basepostgres.txt"
+  su - postgres -c "pg_dump $basepostgres > $BACKUP_DIR/$basepostgres.txt"
 
-  cd $PBACKUP/admsjp-$DATA/
+  cd $BACKUP_DIR
 
   tar -czvf $basepostgres.tar.gz $basepostgres.txt
 		
   sha1sum $basepostgres.tar.gz > $basepostgres.sha1
 
   rm -rf $basepostgres.txt
-
-	cd /
-
 done
 
 DAYOFWEEK=$(date +"%u")
 if [ "${DAYOFWEEK}" -eq 7  ];  then
-
   # Otimizacao das tabelas
   su - postgres -c "vacuumdb -a -f -z"
   # Backup de todo o banco
-  su - postgres -c "pg_dumpall > $PBACKUP/admsjp-$DATA/postgres_tudo.txt"
+  su - postgres -c "pg_dumpall > $BACKUP_DIR/postgres_tudo.txt"
 
-  cd ${PBACKUP}/${DATA}/postgres/
+  cd $BACKUP_DIR
 
   tar -czvf postgres_tudo.tar.gz postgres_tudo.txt
    
   sha1sum postgres_tudo.tar.gz > postgres_tudo.sha1
 
   rm -f postgres_tudo.txt  
-
 fi
-
 
 exit 0
